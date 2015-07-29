82cb26d5d895fb245fa1dd853313302288a43406
kesavan <kesavan.g@cloudenablers.com>
2015-07-29 16:57:07 +0530

    7-zip

---
+++ 7-zip/CHANGELOG.md
@@ -0,0 +1,7 @@
+7-zip Cookbook CHANGELOG
+========================
+This file is used to list changes made in each version of the 7-zip cookbook.
+
+1.0.0
+-----
+- initial release
---
+++ 7-zip/metadata.rb
@@ -0,0 +1,10 @@
+name             "7-zip"
+maintainer       "nagalakshmi_n"
+maintainer_email "nagalakshmi.n@cloudenablers.com"
+license          "Apache 2.0"
+description      "Installs/Configures the 7-zip file archiver"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.0.0"
+supports         "windows"
+
+depends          "windows", ">= 1.2.2"
